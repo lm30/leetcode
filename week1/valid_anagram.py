df708ee5b2eac52da384669c4b74b8d548d8e3f1
Laura Matos <lm3081@columbia.edu>
2019-06-23 19:56:51 -0400

    week1 valid anagrams complete

---
+++ week1/valid_anagram.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+
+        Faster than 90% submissions
+        Less memory than 5% submissions
+        """
+        if len(s) != len(t):
+            return False
+        
+        s_map = {}
+        t_map = {}
+        for i in range(len(s)):
+            if s[i] in s_map:
+                s_map[s[i]] += 1
+            else:
+                s_map[s[i]] = 1
+            if t[i] in t_map:
+                t_map[t[i]] += 1
+            else:
+                t_map[t[i]] = 1
+        
+        for key in s_map:
+            if key not in t_map:
+                return False
+            elif s_map[key] != t_map[key]:
+                return False
+        
+        return True
