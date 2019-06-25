4bef3dceb18034de7c205f506012ad8a318f622e
lm30 <lm3081@columbia.edu>
2019-06-25 17:12:20 -0400

    Update single_number.py

--- week1/single_number.py
+++ week1/single_number.py
@@ -8,11 +8,20 @@ class Solution(object):
 
         Faster than 6% submissions
         Less memory than 68% submissions
+        
+        When using hashes, faster than 72% submissions
+        Less memory than 23% submissions
         """
-        unique_list = []
+#         unique_list = []
+#         for num in nums:
+#             if num not in unique_list:
+#                 unique_list.append(num)
+#             else:
+#                 unique_list.remove(num)
+#         return unique_list[0]
+
+        unique_h = {}
         for num in nums:
-            if num not in unique_list:
-                unique_list.append(num)
-            else:
-                unique_list.remove(num)
-        return unique_list[0]
+            if unique_h.pop(num, 'not_found') == 'not_found': # for default keyerror raised if key not found
+                unique_h[num] = 1
+        return unique_h.popitem()[0]
